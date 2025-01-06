ab0410b3ff0ca2905c0d1fbfde02536d92311d99
Rivindu_Pabasara <101160321+Rivindu10@users.noreply.github.com>
2025-01-06 23:01:15 +0530

    create common header for login

---
+++ uovstudentcare/components/login.js
@@ -0,0 +1,11 @@
+import { Text , View} from "react-native";
+
+const Login = () => {
+    return (
+      <view>
+         <Text h1>STUDENT LOGIN</Text> 
+      </view>
+    );
+  };
+  
+  export default Login;
